af0980493d8e818b88d12d897f235ac4a7aa3716
dopamine <88769856+LeTanTru@users.noreply.github.com>
2024-10-24 15:18:24 +0700

    Update func.sql

--- func.sql
+++ func.sql
@@ -1,206 +1,158 @@
-
 -- 3.2.4. Hàm tìm kiếm dữ liệu
--- 3.2.4.1. Bảng room_type
---Tìm kiếm theo tên
-
-CREATE FUNCTION fn_SearchNameRoomType(@ten NVARCHAR(50))
+-- 3.2.4.1. Bảng room
+--Tìm kiếm theo mã phòng
+CREATE FUNCTION fn_SearchRoomById
+(
+   	@room_id VARCHAR(20)
+)
 RETURNS TABLE
 AS
 RETURN
 (
-    SELECT *
-    FROM room_type
-    WHERE room_type_name = @ten
+   	SELECT
+       room_id,
+       room_name,
+       status,
+       room_type_id,
+       manager_id
+   	FROM room
+   	WHERE room_id = @room_id
 );
 GO
---Tìm kiếm theo mã loại phòng
-CREATE FUNCTION fn_SearchIDRoomType(@id VARCHAR(20))
+
+--Tìm kiếm theo tên phòng
+CREATE FUNCTION fn_SearchRoomByName
+(
+	@room_name NVARCHAR(50)
+)
 RETURNS TABLE
 AS
 RETURN
 (
-    SELECT *
-    FROM room_type
-    WHERE room_type_id = @id
+	SELECT
+    	room_id,
+    	room_name,
+    	[status],
+    	room_type_id,
+    	manager_id
+	FROM room
+	WHERE room_name = @room_name
 );
 GO
 
---3.2.4.2. Bảng service_usage_record
-CREATE FUNCTION fn_SearchServiceUsageRecord(@id VARCHAR(20))
+
+--3.2.4.2. Bảng customer
+-- Tìm customer theo tên
+CREATE FUNCTION fn_FindCustomerByName (
+	@full_name VARCHAR(255)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT *
+	FROM customer
+	WHERE full_name LIKE '%' + @full_name + '%'
+);
+GO
+--Tìm customer theo id
+CREATE FUNCTION fn_FindCustomerByIDNumber (
+ 	@id_number VARCHAR(20)
+)
 RETURNS TABLE
 AS
 RETURN
 (
  	SELECT *
- 	FROM service_usage_record
- 	WHERE service_usage_id = @id
+ 	FROM dbo.customer
+ 	WHERE identification_number = @idNumber
 );
 GO
---3.2.4.3. Bảng room 
---Tìm kiếm theo mã phòng
-CREATE PROCEDURE sp_SearchRoomById
-    @room_id VARCHAR(20)
-AS
-BEGIN
- 
-    BEGIN TRY
-       IF EXISTS (SELECT 1 FROM room WHERE room_id= @room_id)
-       BEGIN
-    	  	SELECT
-        	  room_id,
-             room_name,
-        	   status,
-        	  room_type_id,
-        	  manager_id
-    	  	FROM room
-    	  	WHERE room_id = @room_id;
-       END
-    END TRY
-    BEGIN CATCH
-       DECLARE @ErrorMessage NVARCHAR(4000);
-       DECLARE @ErrorSeverity INT;
-       DECLARE @ErrorState INT;
-       SELECT
-    	  	@ErrorMessage = ERROR_MESSAGE(),
-    	  	@ErrorSeverity = ERROR_SEVERITY(),
-    	  	@ErrorState = ERROR_STATE();
-       RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
-    END CATCH
-END;
-GO
 
---Tìm kiếm theo tên phòng
-CREATE PROCEDURE sp_SearchRoomByName
-    @room_name NVARCHAR(50)
+
+--3.2.4.3. Bảng staff 
+--Tìm kiếm id của staff theo username
+CREATE FUNCTION fn_GetStaffIdByUsername(@username VARCHAR(20))
+RETURNS VARCHAR(20)
 AS
 BEGIN
+   	DECLARE @staff_id VARCHAR(20);
  
-    BEGIN TRY
-       IF EXISTS (SELECT 1 FROM room WHERE room_name= @room_name)
-       BEGIN
-    	  	SELECT
-        	  room_id,
-                room_name,
-        	   [status],
-        	  room_type_id,
-        	  manager_id
-    	  	FROM room
-    	  	WHERE room_name = @room_name;
-       END
-    END TRY
-    BEGIN CATCH
-       DECLARE @ErrorMessage NVARCHAR(4000);
-       DECLARE @ErrorSeverity INT;
-       DECLARE @ErrorState INT;
-       SELECT
-    	  	@ErrorMessage = ERROR_MESSAGE(),
-    	  	@ErrorSeverity = ERROR_SEVERITY(),
-    	  	@ErrorState = ERROR_STATE();
-       RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
-    END CATCH
+   	SELECT @staff_id = staff_id
+   	FROM staff
+   	WHERE username = @username
+ 
+   	return @staff_id;
 END;
 GO
 
 
---3.2.4.4. Bảng customer 
--- Tìm kiếm theo tên khách hàng
-use hotel_management
-CREATE OR ALTER FUNCTION fn_FindtoCustomer (
-	@full_name NVARCHAR(255)
-)
-RETURNS TABLE
+--Tìm kiếm full_name của nhân viên theo username
+CREATE FUNCTION fn_GetStaffFullNameByUsername(@username VARCHAR(20))
+RETURNS NVARCHAR(255)
 AS
-RETURN
-(
-	SELECT *
-	FROM dbo.customer
-	WHERE full_name LIKE '%' + @full_name + '%'
-);
+BEGIN
+   	DECLARE @full_name NVARCHAR(255)
+ 
+   	SELECT @full_name = full_name
+   	FROM staff
+   	WHERE username = @username
+ 
+   	return @full_name;
+END;
 GO
 
---Tìm kiếm theo mã khách hàng
-CREATE FUNCTION fn_FindCustomer (
-	@customer_id VARCHAR(20)
-)
-RETURNS TABLE
-AS
-RETURN
-(
-	SELECT *
-	FROM dbo.customer
-	WHERE customer_id = @customer_id
-);
-GO
-
---3.2.4.5. Bảng service 
-
-CREATE FUNCTION fn_FindServiceById (
-    @service_id VARCHAR(20)
-)
-RETURNS TABLE
-AS
-RETURN
-(
-    SELECT *
-    FROM dbo.service
-    WHERE service_id = @service_id
-);
-GO
-
---3.2.4.6. Tính tổng doanh thu theo ngày (ví dụ nhập vào 1 ngày và tính tổng các hóa đơn hôm đó)
+-- 3.2.4.4. Tính tổng doanh thu theo ngày 
 CREATE FUNCTION fn_CalculateTotalRevenueByDate
 (
-	@day INT,
-	@month INT,
-	@year INT
+ 	@day INT,
+ 	@month INT,
+ 	@year INT
 )
 RETURNS INT
 AS
 BEGIN
-	DECLARE @totalRevenue INT;
+ 	DECLARE @totalRevenue INT;
  
-	SELECT @totalRevenue = SUM(CAST(total AS INT)) -- Chuyển 'total' về kiểu INT
-	FROM bill
-	WHERE DAY(created_at) = @day
-	AND MONTH(created_at) = @month
-	AND YEAR(created_at) = @year;
+ 	SELECT @totalRevenue = SUM(CAST(total AS INT)) -- Chuyển 'total' về kiểu INT
+ 	FROM bill
+ 	WHERE DAY(created_at) = @day
+ 	AND MONTH(created_at) = @month
+ 	AND YEAR(created_at) = @year;
  
-	-- Trả về giá trị tổng doanh thu, nếu không có giá trị thì trả về 0
-	RETURN ISNULL(@totalRevenue, 0);
+ 	-- Trả về giá trị tổng doanh thu, nếu không có giá trị thì trả về 0
+ 	RETURN ISNULL(@totalRevenue, 0);
 END;
 GO
 
-SELECT dbo.fn_CalculateTotalRevenueByDate(1, 1, 2024);
-
-
---3.2.4.7. Tính tổng doanh thu theo tháng (ví dụ nhập vào 1 tháng và tính tổng các hóa đơn trong tháng đó)
-
-CREATE FUNCTION fn_GetDailyRevenue
+-- 3.2.4.5. Tính tổng doanh thu theo tháng 
+CREATE FUNCTION fn_GetMonthlyRevenue
 (
-	@month INT,
-	@year INT
+   	@month INT,
+   	@year INT
 )
 RETURNS @DailyRevenueTable TABLE
 (
-	[day] INT,
-	total INT
+   	[day] INT,
+   	total INT
 )
 AS
 BEGIN
-	-- Chèn dữ liệu vào bảng trả về
-	INSERT INTO @DailyRevenueTable([day], total)
-	SELECT
-    	DAY(created_at) AS [day],
-    	SUM(total) AS total
-	FROM Bill
-	WHERE MONTH(created_at) = @month AND YEAR(created_at) = @year
-	GROUP BY DAY(created_at)
-	ORDER BY [day];
+   	-- Chèn dữ liệu vào bảng trả về
+   	INSERT INTO @DailyRevenueTable([day], total)
+   	SELECT
+       DAY(created_at) AS [day],
+       SUM(total) AS total
+   	FROM Bill
+   	WHERE MONTH(created_at) = @month AND YEAR(created_at) = @year
+   	GROUP BY DAY(created_at)
+   	ORDER BY [day];
  
-	RETURN;
+   	RETURN;
 END;
 GO
---3.2.4.8. Tính tổng doanh thu theo quý (ví dụ nhập vào 1 quý và tính tổng các hóa đơn trong quý đó)
+
+-- 3.2.4.6. Tính tổng doanh thu theo quý
 CREATE FUNCTION fn_TotalRevenueByQuarter
 (
 	@quarter INT,
@@ -211,7 +163,7 @@ AS
 RETURN
 (
 	SELECT
-    	MONTH(created_at) AS [month],
+    	MONTH(created_at) AS month,
     	SUM(total) AS total_revenue
 	FROM bill
 	WHERE YEAR(created_at) = @year
@@ -226,88 +178,238 @@ RETURN
 );
 GO
 
---3.2.4.9. Tính tổng doanh thu theo năm (ví dụ nhập vào 1 năm và tính tổng các hóa đơn trong năm đó)
+-- 3.2.4.7. Tính tổng doanh thu theo năm (ví dụ nhập vào 1 năm và tính tổng các hóa đơn trong năm đó)
 CREATE FUNCTION fn_TotalRevenueByYear
 (
-	@year INT
+   	@year INT
 )
 RETURNS TABLE
 AS
 RETURN
 (
-	SELECT
-    	MONTH(created_at) AS [Month],
-    	SUM(total) AS TotalRevenue
-	FROM
-    	bill
-	WHERE
-    	YEAR(created_at) = @year
-	GROUP BY
-    	MONTH(created_at)
+   	SELECT
+       MONTH(created_at) AS [Month],
+       SUM(total) AS TotalRevenue
+   	FROM
+       bill
+   	WHERE
+       YEAR(created_at) = @year
+   	GROUP BY
+       MONTH(created_at)
 );
 GO
 
---3.2.4.10. Kiểm tra login
-CREATE  FUNCTION fn_CheckLogin(@username VARCHAR(50), @password VARCHAR(255))
+-- 3.2.4.8. Kiểm tra login
+CREATE FUNCTION fn_CheckLogin (
+	@username VARCHAR(50),
+	@password VARCHAR(255)
+)
 RETURNS INT
 AS
 BEGIN
-    DECLARE @result INT;
-    DECLARE @role VARCHAR(20);
-
-    -- Kiểm tra nếu tài khoản tồn tại và lấy vai trò
-    SELECT @role = role
-    FROM account
-    WHERE username = @username AND [password] = @password;
-
-    IF @role IS NOT NULL
-    BEGIN
-        IF @role = 'manager'
-        BEGIN
-            SET @result = 1;  -- Quản lý
-        END
-        ELSE IF @role = 'receptionist'
-        BEGIN
-            SET @result = 0;  -- Lễ tân
-        END
-    END
-    ELSE
-    BEGIN
-        SET @result = -1;  -- Tài khoản không tồn tại
-    END
-
-    RETURN @result;
+	DECLARE @result INT;
+	DECLARE @role VARCHAR(20);
+	
+	-- Kiểm tra nếu tài khoản tồn tại và lấy vai trò
+	SELECT @role = role
+	FROM account
+	WHERE username = @username AND [password] = @password;
+ 
+	IF @role IS NOT NULL
+	BEGIN
+    	IF @role = 'manager'
+    	BEGIN
+        	SET @result = 1;  -- Quản lý
+    	END
+    	ELSE IF @role = 'receptionist'
+    	BEGIN
+        	SET @result = 0;  -- Lễ tân
+    	END
+	END
+	ELSE
+	BEGIN
+    	SET @result = -1;  -- Tài khoản không tồn tại
+	END
+ 
+	RETURN @result;
 END;
+
+-- 3.2.4.9. Hàm lấy thông tin bill của phòng đang tìm.
+CREATE FUNCTION fn_GetBillInfoByRoomId
+(
+    @room_id  VARCHAR(20)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT bill.*
+    FROM bill
+    INNER JOIN booking_record b ON b.customer_id = bill.customer_id
+    WHERE b.room_id = @roomId
+  	AND b.actual_check_out_time IS NULL
+  	AND bill.created_at IS NULL
+);
 GO
 
---3.2.4.11. Lấy ra tên staff theo username
-CREATE  FUNCTION fn_GetStaffIdByUsername(@username VARCHAR(20))
+-- 3.2.4.10. Hàm lấy thông tin phiếu đặt phòng
+CREATE FUNCTION fn_GetBookingRecordByRoomIdToCheckOut
+(
+    @room_id  VARCHAR(20)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT
+    	br.booking_record_id,
+    	br.booking_time,
+    	br.status,
+    	br.expected_check_in_time,
+    	br.expected_check_out_time,
+    	br.actual_check_in_time,
+    	br.actual_check_out_time,
+    	br.receptionist_id,
+    	s.full_name AS receptionist_name,
+    	br.customer_id,
+    	c.full_name AS customer_name,
+    	br.room_id,
+    	r.room_name
+    FROM
+    	booking_record br
+    LEFT JOIN
+    	staff s ON br.receptionist_id = s.staff_id
+    LEFT JOIN
+    	customer c ON br.customer_id = c.customer_id
+    LEFT JOIN
+    	room r ON br.room_id = r.room_id
+    WHERE
+    	br.room_id = @room_id
+       	AND br.status = 'staying'
+);
+GO
+
+-- 3.2.4.11. Hàm lấy thông tin phiếu dịch vụ thông qua mã phòng
+CREATE FUNCTION fn_GetServiceUsageInfoByRoomId
+(
+    @roomId  VARCHAR(20)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT s.*
+    FROM service_usage_record s
+ 	JOIN booking_record b ON s.booking_id = b.booking_record_id
+    WHERE b.room_id = @roomId AND b.actual_check_out_time IS NULL
+);
+GO
+
+-- 3.2.4.12. Hàm tìm các phòng đã đặt của 1 khách hàng qua mã định danh
+CREATE FUNCTION fn_GetDepositedRoomsByIdNumber(@id_number VARCHAR(20))
+RETURNS TABLE
+AS
+RETURN
+(
+ 	SELECT
+       	br.booking_record_id,
+       	r.room_name,
+       	rt.room_type_name,
+       	r.status,
+       	c.full_name,
+       	c.identification_number,
+       	br.expected_check_in_time,
+       	br.expected_check_out_time,
+       	rt.capacity,
+       	rt.cost_per_day
+ 	FROM
+       	room r
+       	JOIN room_type rt ON r.room_type_id = rt.room_type_id
+       	JOIN booking_record br ON r.room_id = br.room_id
+       	JOIN customer c ON br.customer_id = c.customer_id
+ 	WHERE
+       	c.identification_number = @id_number AND r.status = 'deposited');
+GO
+
+-- 3.2.4.13. Hàm lấy thông tin mã phiếu đặt phòng đang được áp dụng qua mã phòng
+CREATE FUNCTION fn_GetBookingRecordIdByRoomIdCustomerId(
+    @room_id VARCHAR(20)
+)
 RETURNS VARCHAR(20)
 AS
 BEGIN
-	DECLARE @staff_id VARCHAR(20);
-
-	SELECT @staff_id = staff_id
-	FROM staff
-	WHERE username = @username
-
-	return @staff_id;
+    DECLARE @booking_record_id VARCHAR(20);
+ 
+    -- Lấy booking_record_id với điều kiện theo room_id, customer_id, và status
+    SELECT @booking_record_id = booking_record_id
+    FROM booking_record
+    WHERE room_id = @room_id
+ 
+    -- Trả về booking_record_id nếu tìm thấy
+    RETURN @booking_record_id;
 END;
 GO
 
---3.2.4.12. Lấy ra tên staff theo username
-CREATE FUNCTION fn_GetStaffFullNameByUsername(@username VARCHAR(20))
-RETURNS NVARCHAR(255)
+-- 3.2.4.14. Lấy ra tên staff theo username
+CREATE FUNCTION fn_GetServiceUsageByBookingId(@booking_record_id VARCHAR(20))
+RETURNS TABLE
 AS
-BEGIN
-	DECLARE @full_name NVARCHAR(255)
+RETURN
+(
+    SELECT
+    	s.service_id,
+    	s.service_name,
+    	s.price,
+    	SUM(sur.quantity) AS quantity,
+    	SUM(sur.quantity * s.price) AS total
+    FROM
+    	service s
+    LEFT JOIN
+    	service_usage_record sur ON s.service_id = sur.service_id
+    WHERE
+    	sur.booking_id = @bookingRecordId
+    GROUP BY
+    	s.service_id, s.service_name, s.price
+);
+GO 
 
-	SELECT @full_name = full_name
-	FROM staff
-	WHERE username = @username
+-- 3.2.4.15. Hàm lấy hóa đơn phòng theo mã phiếu đặt phòng.
+CREATE FUNCTION fn_GetRoomBillByBookingRecordId
+(
+    @booking_record_id VARCHAR(20)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT
+    	r.room_name,
+    	rt.cost_per_day,
+    	br.expected_check_in_time,
+    	br.expected_check_out_time,
+    	DATEDIFF(DAY, br.expected_check_in_time, br.expected_check_out_time) * rt.cost_per_day AS total
+    FROM
+    	booking_record br
+    JOIN
+    	room r ON br.room_id = r.room_id
+    JOIN
+    	room_type rt ON r.room_type_id = rt.room_type_id
+    WHERE
+    	br.booking_record_id = @booking_record_id
+)
+GO
 
-	return @full_name;
-END;
+-- 3.2.4.16. Hàm lấy mã hóa đơn thông qua mã khách.	
+CREATE FUNCTION fn_getBillIDByCustomerID
+(
+    @customer_id VARCHAR(20)
+)
+RETURNS table
+AS
+return (
+    SELECT *
+    FROM bill b
+    WHERE b.customer_id = @customer_id
+);
+GO
 
-
-SELECT * FROM fn_FindtoCustomer('A');
